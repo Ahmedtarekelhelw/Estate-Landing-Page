c60ae4efecde0f73e542edb0c68707ddc0b880f1
Ahmedtarekelhelw <elhelwahmed4@gmail.com>
2022-08-16 17:52:03 +0200

    modify title of page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-//click on toggle to show link in small window 
-let links = document.querySelector('.links')
-let toggleMenu = document.querySelector('.toggle-menu')
-
+//click on toggle to show link in small window
+let links = document.querySelector(".links");
+let toggleMenu = document.querySelector(".toggle-menu");
 
 toggleMenu.onclick = function () {
-    links.classList.toggle('show')
-    toggleMenu.classList.toggle('active')
-}
+  links.classList.toggle("show");
+  toggleMenu.classList.toggle("active");
+};
 
 //another way to do this
 
@@ -15,37 +14,30 @@ toggleMenu.onclick = function () {
 //     links.classList.toggle('show')
 // }
 
-
-
 //close nav-link when click on links
-let navLink = document.querySelectorAll('.nav-link')//bring all links with this class
+let navLink = document.querySelectorAll(".nav-link"); //bring all links with this class
 
 // for loop on links
-navLink.forEach(n => n.addEventListener('click', close) )
+navLink.forEach((n) => n.addEventListener("click", close));
 
-function close () {
-    links.classList.remove('show')
-    toggleMenu.classList.remove('active')
+function close() {
+  links.classList.remove("show");
+  toggleMenu.classList.remove("active");
 }
-
-
-
-
 
 //decrease padding of header when scrolling
-let con = document.querySelector('.container')
+let con = document.querySelector(".container");
 window.onscroll = function () {
-    // console.log(this.scrollY)
-    if (this.scrollY >=50) {
-        con.classList.add('small-nav')
-    }else {
-        con.classList.remove('small-nav')
-    }
-}
+  if (this.scrollY >= 50) {
+    con.classList.add("small-nav");
+  } else {
+    con.classList.remove("small-nav");
+  }
+};
 
 //scroll button
-let scroll = document.querySelector('.scroll')
+let scroll = document.querySelector(".scroll");
 
 scroll.onclick = function () {
-    window.scrollTo(0, 720 );
-}
+  window.scrollTo(0, 720);
+};
